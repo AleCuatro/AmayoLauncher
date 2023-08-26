51cc8f512d5444011439c1022c5149f22167e9f5
Ale <antoniomlllzzz@hotmail.com>
2023-08-26 18:16:40 -0500

    AmayoLauncher

---
+++ app/assets/js/distromanager.js
@@ -0,0 +1,17 @@
+const { DistributionAPI } = require('helios-core/common')
+
+const ConfigManager = require('./configmanager')
+
+// Old WesterosCraft url.
+// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://150.136.70.1:27022/config'
+
+const api = new DistributionAPI(
+    ConfigManager.getLauncherDirectory(),
+    null, // Injected forcefully by the preloader.
+    null, // Injected forcefully by the preloader.
+    exports.REMOTE_DISTRO_URL,
+    false
+)
+
+exports.DistroAPI = api
